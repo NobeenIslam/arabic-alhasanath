e21729b604a6e1082c9046566493a68570c8659c
Nobeen Islam <nobeen.islam@xdesign.com>
2024-08-20 19:37:49 +0100

    Set up colours and font

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,11 +7,35 @@ const config: Config = {
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
+    screens: {
+      sm: "640px",
+      md: "960px",
+      lg: "1120px",
+      xl: "1512px",
+    },
+
     extend: {
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      },
+      fontFamily: {
+        sans: [`var(--font-montserrat)`],
+      },
+      colors: {
+        specialOrange: "var(--special-orange)",
+        specialBlue: "var(--special-blue)",
+        heroGradient: "var(--hero-gradient)",
+        buttonBlue: "var(--button-blue)",
+        buttonPink: "var(-button-pink)",
+        specialIconRed: "var(--special-icon-red)",
+        specialFormBlue: "var(--special-form-blue)",
+        specialCardGrey: "var(--special-card-grey)",
+        specialBackgroundGreen: "var(--special-background-green)",
+        specialBackgroundRed: "var(--special-background-red)",
+        specialBackgroundPink: "var(--special-background-pink)",
+
       },
     },
   },
