4d117166f4d54700c0fc0d189ee264431c89d5f6
NobeenIslam <n.islam.1@outlook.com>
2024-08-28 21:57:15 +0100

    Add man silhouette for testimonials

--- app/Data.ts
+++ app/Data.ts
@@ -84,7 +84,7 @@ export const courseStructureBlockData: CourseStructureBlockProps = {
 const dummyTestimonialEntry: Testimonial = {
   quote:
     "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book",
-  image: "/man.jpg", // Replace with actual path
+  image: "/silhouette.jpeg", // Replace with actual path
   name: "Nobeen Islam",
 };
 
